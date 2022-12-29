e6edd993b0c11479b4e348f7e046a450c0177c27
KangDHGit <eogks0323@gmail.com>
2022-12-29 12:06:12 +0900

    12/29 복합자료형 스택 구조 구현 완료

--- algorithm/Chaper4_Stack/stack_program_complex.cpp
+++ algorithm/Chaper4_Stack/stack_program_complex.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 
 using std::cout;
@@ -34,11 +35,56 @@ void element::print()
 }
 #pragma endregion
 
+class stackType
+{
+	element data[MAX_STACK_SIZE];
+	int top;
+public:
+	stackType() : top(-1) {};
+	bool is_full();
+	bool is_empty();
+	void push(element item);
+	element pop();
+	element peek();
+};
+
+#pragma region stackType define
+bool stackType::is_full() { if (top == MAX_STACK_SIZE) return true; else return false; }
+bool stackType::is_empty() { if (top == -1) return true; else return false; }
+void stackType::push(element item)
+{
+	if (is_full()) { cout << "스택 포화에러" << endl; exit(1); }
+	else { top++; data[top] = item; }
+}
+element stackType::pop()
+{
+	if (is_empty()) { cout << "스택 공백에러" << endl; exit(1); }
+	else { return data[top--]; }
+}
+element stackType::peek()
+{
+	if (is_empty()) { cout << "스택 공백에러" << endl; exit(1); }
+	else { return data[top]; }
+}
+#pragma endregion
+
 
 int main()
 {
-	element student{ 1,"강대한", "김포" };
-	student.print();
+	stackType class_A;
+
+	element student0{ 1,"강대한", "김포" };
+	element student1{ 2,"홍길동", "서울" };
+	element student2{ 3,"김철수", "일산" };
+
+	class_A.push(student0);
+	class_A.push(student1);
+	class_A.push(student2);
+
+	class_A.pop().print();
+	class_A.pop().print();
+	class_A.pop().print();
+	class_A.pop().print();
 	
 	return 0;
 }
