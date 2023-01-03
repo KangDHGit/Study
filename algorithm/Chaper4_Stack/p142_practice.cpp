198f47cf4e82900c5a78d0d8caf4ca9b55fb1efc
KangDHGit <eogks0323@gmail.com>
2023-01-03 18:13:07 +0900

    01/03 chapter04 15번 경로출력 문제 완료(최단경로 출력으로 변형)

--- algorithm/Chaper4_Stack/p142_practice.cpp
+++ algorithm/Chaper4_Stack/p142_practice.cpp
@@ -215,13 +215,293 @@ public:
 	}
 };
 #pragma endregion
+#pragma region question_13
+class stackType_13 {
+	int* data;
+	int capacity;
+	int top;
+public:
+	void delete_stack() { delete[] data; }
+	void init();
+	stackType_13() { init(); }
+	bool is_full() { return top == capacity - 1; }
+	bool is_empty() { return top == -1; }
+	bool resize(int& cap);
+	void push(int item);
+	int pop();
+	int peek();
+	~stackType_13() { delete_stack(); }
+};
+void stackType_13::init() {
+	if (data != NULL) { delete_stack(); }
+	data = new int[1];
+	capacity = 1; top = -1;
+}
+bool stackType_13::resize(int& cap) {
+	int* new_mem = new int[cap];
+	if (new_mem == NULL) { return false; }
+	else {
+		std::copy(&data[0], &data[top + 1], new_mem);
+		delete_stack();
+		data = new_mem;
+		return true;
+	}
+}
+void stackType_13::push(int item) {
+	if (is_full()) {
+		int cap = capacity * 2;
+		if (resize(cap))
+			capacity = cap;
+		else { cout << "스택 공간확보 실패" << endl; }
+	}
+	top++; data[top] = item;
+}
+int stackType_13::pop() {
+	if (is_empty()) { cout << "스택 공백에러" << endl; exit(1); }
+	else { return data[top--]; }
+}
+int stackType_13::peek() {
+	if (is_empty()) { cout << "스택 공백에러" << endl; exit(1); }
+	else { return data[top]; }
+}
+
+class question_13 {
+	char input[20];
+	stackType_13 stack;
+public:
+	void run();
+};
+void question_13::run() {
+	cout << "정수를 입력하시오 : "; cin >> input;
+	/*int len = sizeof(input);
+	for (int i = 0; i < len; i++)
+	{
+		int num = input[i];
+		if (stack.is_empty()) { stack.push(num); }
+		else if (stack.peek() == (int)input[i]) { continue; }
+		else { stack.push(num); }
+	}*/
+	int i = 0;
+	while (input[i] != NULL)
+	{
+		int num = input[i] - 48;
+		i++;
+		if (stack.is_empty()) { stack.push(num); }
+		else if (stack.peek() == num) { continue; }
+		else { cout << stack.pop(); stack.push(num); }
+	}
+
+	while (!stack.is_empty())
+	{
+		cout << stack.pop();
+	}
+}
+#pragma endregion
+#pragma region question_14
+struct element {
+	short r;
+	short c;
+public:
+	element() {};
+	element(short row, short col) : r(row), c(col) {};
+	void print() { cout << r << ", " << c << " "; }
+};
+
+class stackType
+{
+	element* data;		//동적할당을 위해 포인터로 선언
+	int capacity;		//현재크기
+	int top;
+public:
+	void delete_stack() { delete[] data; cout << "데이터 삭제" << endl; }
+	void init_stack();
+	bool resize_stack(int& cap);
+	stackType() { init_stack(); };
+	bool is_full() const;
+	bool is_empty() const;
+	void push(element item);
+	element pop();
+	element peek();
+	void printstack();
+	~stackType() { delete_stack(); }
+};
+
+#pragma region stackType define
+void stackType::init_stack() {
+	if (data != NULL) { delete[] data; cout << "기존 데이터를 삭제 했습니다." << endl; }
+	data = new element[1];
+	top = -1; capacity = 1;
+	cout << "스택을 초기화 했습니다." << endl;
+}
+bool stackType::resize_stack(int& cap) {
+	element* new_mem = new element[cap];
+	if (new_mem == NULL) return false;
+	else {
+		std::copy(&data[0], &data[top + 1], new_mem);
+		delete[] data;
+		data = new_mem;
+		return true;
+	}
+}
+bool stackType::is_full() const { return top == capacity - 1; }
+bool stackType::is_empty() const { return top == -1; }
+void stackType::push(element item) {
+	if (is_full()) {
+		/*cout << "스택 공간을 추가확보 합니다." << endl;*/
+		int cap = capacity * 2;
+		if (resize_stack(cap)) {
+			/*cout << "공간 추가확보에 성공했습니다." << endl;*/
+			capacity = cap;
+		}
+		else { cout << "공간 추가확보에 실패했습니다." << endl; return; }
+	}
+	top++;
+	data[top] = item;
+	//item.print();
+}
+element stackType::pop() {
+	if (is_empty()) { cout << "스택 공백에러!" << endl; exit(1); }
+	else { return data[top--]; }
+}
+element stackType::peek() {
+	if (is_empty()) { cout << "스택 공백에러!" << endl; exit(1); }
+	else { return data[top]; }
+}
+void stackType::printstack() {
+	for (int i = top + 1; i >= 0; i--)
+		data[i].print();
+	cout << endl;
+}
+#pragma endregion
+
+class maze
+{
+	const static int size = 6;
+	element entry;
+public:
+	char map[size][size] = {
+		{'1','1','1','1','1','1'},
+		{'e','0','1','0','0','1'},
+		{'1','0','0','0','1','1'},
+		{'1','0','1','0','1','1'},
+		{'1','0','1','0','0','x'},
+		{'1','1','1','1','1','1'}
+	};
+public:
+	maze() : entry(1, 0) {};
+	element getEntry() { return entry; }
+	void print_maze();
+};
+#pragma region maze_define
+void maze::print_maze() {
+	cout << endl;
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 6; j++)
+			cout << map[i][j] << " ";
+		cout << endl;
+	}
+}
+#pragma endregion
+
+class pathManager {
+	int around_count = 0;
+	int index = 0;
+	element paths[50];
+	stackType_13 index_stack;
+public:
+	int get_around() { return around_count; }
+	void add_around() { around_count++; }
+	void init_around() { around_count = 0; }
+	void add_path(element path) {
+		if (index < 50) { 
+			paths[index++] = path;
+			if (around_count > 1)
+				index_stack.push(index);
+
+			init_around();
+		}
+	}
+	void delete_path() { index = index_stack.pop(); }
+	void print_path();
+};
+void pathManager::print_path() {
+	cout << "최단경로는 : ";
+	for (int i = 0; i < index; i++) { paths[i].print(); cout << " > "; }
+	cout << endl;
+}
+
+
+class mazeGame {
+	stackType stack;	//갈수있는 위치를 저장할 스택자료구조 변수
+	element here;		//미로에서의 현재 내 위치
+	maze miro;			//미로
+	pathManager manager;//최단경로를 저장해줄 매니저
+public:
+	void push_loc(int r, int c);	//갈수있는 위치인지 판단하고 스택에 저장하는 함수
+	void maze_search();				//미로찾기 함수
+};
+
+#pragma region mazeGame_define
+void mazeGame::push_loc(int r, int c) {
+	if (miro.map[r][c] != '.' && miro.map[r][c] != '1') {
+		element temp(r, c);
+		stack.push(temp);
+		manager.add_around();
+	}
+}
+void mazeGame::maze_search() {
+	miro.print_maze();
+	here = miro.getEntry();
+
+	while (miro.map[here.r][here.c] != 'x')
+	{
+		int r = here.r, c = here.c;
+		miro.map[r][c] = '.';
+		miro.print_maze();
+		push_loc(r + 1, c);
+		push_loc(r - 1, c);
+		push_loc(r, c - 1);
+		push_loc(r, c + 1);
+		if (!stack.is_empty()) {
+			if (manager.get_around() > 0)
+				manager.add_path(here);
+			else
+				manager.delete_path();
+
+			here = stack.pop();
+		}
+		else { cout << "미로찾기 실패!" << endl; break; }
+	}
+
+	miro.map[here.r][here.c] = '.';
+	
+	manager.add_path(here);
+	manager.print_path();
+	cout << "미로찾기 성공!" << endl;;
+}
+#pragma endregion
+
+class question_15 {
+	mazeGame game;
+public:
+	void run() {
+		game.maze_search();
+	}
+};
+#pragma endregion
+
 
 int main()
 {
 	//run_Question_10()
 	/*question_11 q11;
 	q11.run();*/
-	qusetion_12 q12;
-	q12.run();
+	/*qusetion_12 q12;
+	q12.run();*/
+	/*question_13 q13;
+	q13.run();*/
+	question_15 q15;
+	q15.run();
 	return 0;
 }
