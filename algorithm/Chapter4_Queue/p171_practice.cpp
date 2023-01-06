323b71514c1f8db67191dc87756a1f1c35777372
KangDHGit <eogks0323@gmail.com>
2023-01-06 11:59:12 +0900

    01/06 Chapter5 연습문제 11번 완료(데크를 활용한 회문 판독)

--- algorithm/Chapter4_Queue/p171_practice.cpp
+++ algorithm/Chapter4_Queue/p171_practice.cpp
@@ -143,12 +143,123 @@ public:
 
 #pragma endregion
 
+//11. 데크를 활용해서 회문 판독하기
+#pragma region question_11
+class dequeType {
+	const static int MAX_SIZE = 10;
+	char data[MAX_SIZE];
+	int rear;
+	int front;
+public:
+	dequeType() { init(); }
+	void init() { front = 0; rear = 0; }
+	bool is_full() { return (rear + 1) % MAX_SIZE == front; }
+	bool is_empty() { return rear == front; }
+	void add_rear(char item) {
+		if (is_full()) { cout << "데크 포화에러" << endl; }
+		else {
+			rear = (rear + 1) % MAX_SIZE;
+			data[rear] = item;
+		}
+	}
+	char delete_front() {
+		if (is_empty()) { cout << "데크 공백에러" << endl; }
+		else {
+			front = (front + 1) % MAX_SIZE;
+			return data[front];
+		}
+	}
+	void add_front(char item) {
+		if (is_full()) { cout << "데크 포화에러" << endl; } 
+		else {
+			data[front] = item;
+			front = (front - 1 + MAX_SIZE) % MAX_SIZE;
+		}
+	}
+	char delete_rear() {
+		if (is_empty()) { cout << "데크 공백에러" << endl; }
+		else{
+			char result = data[rear];
+			rear = (rear - 1 + MAX_SIZE) % MAX_SIZE;
+			return result;
+		}
+	}
+	char peek_front() {
+		if (is_empty()) { cout << "데크 공백에러" << endl; }
+		else {
+			char result = (front + 1) % MAX_SIZE;
+			return data[result];
+		}
+	}
+	char peek_rear() {
+		if (is_empty()) { cout << "데크 공백에러" << endl; }
+		else{
+			return data[rear];
+		}
+	}
+};
+class question_11 {
+	dequeType deque;
+	char str0[7] = "E_y  e";
+	char str1[9] = "ma2dA_m";
+	char str2[9] = "R*a)dAr";
+public:
+	void reword(char in[], char* out, int* outcount) {
+		// (입력 문자열, 출력문자열, 출력문자열의 길이)
+		int len = strlen(in);
+		for (int i = 0; i < len; i++)
+		{
+			char chr = in[i];
+			if (chr >= 'a' && chr <= 'z') {
+				out[(*outcount)++] = chr;
+			}
+			else if (chr >= 'A' && chr <= 'Z') {
+				char low = tolower(chr);
+				out[(*outcount)++] = low;
+			}
+		}
+	}
+
+	bool palindrome(char str[]) {
+		deque.init();
+		char word[10];
+		int wordindex = 0;
+
+		reword(str, word, &wordindex);
+
+		for (int j = 0; j < wordindex; j++)
+		{
+			deque.add_rear(word[j]);
+		}
+		int count = wordindex / 2;
+		for (int i = 0; i < count; i++)
+		{
+			cout << deque.peek_front() << " | " << deque.peek_rear() << endl;
+			if (deque.delete_front() != deque.delete_rear())
+				return false;
+		}
+		return true;
+	}
+
+	void run() {
+		if (palindrome(str0)) { cout << "회문입니다." << endl; }
+		else { cout << "회문이 아닙니다." << endl; }
+		if (palindrome(str1)) { cout << "회문입니다." << endl; }
+		else { cout << "회문이 아닙니다." << endl; }
+		if (palindrome(str2)) { cout << "회문입니다." << endl; }
+		else { cout << "회문이 아닙니다." << endl; }
+	}
+};
+#pragma endregion
+
 
 int main()
 {
 	/*question_9 q9;
 	q9.run();*/
-	question_10 q10;
-	cout << q10.fibo(10) << endl;
+	/*question_10 q10;
+	cout << q10.fibo(10) << endl;*/
+	question_11 q11;
+	q11.run();
 	return 0;
 }
