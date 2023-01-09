e9906262911cb1a41cf36b79b1eeecafbba99ec3
KangDHGit <eogks0323@gmail.com>
2023-01-09 13:32:46 +0900

    Revert "01/08 배열리스트 완료"
    This reverts commit 03fa5ecd9f7922422eff6b37c25eedae5bea9924.

--- algorithm/Linkedd_List1/p178_arrayList.cpp
+++ algorithm/Linkedd_List1/p178_arrayList.cpp
@@ -8,7 +8,7 @@ typedef int element;
 
 class ArrayListType {
 	const static int MAX_LIST_SIZE = 100;
-	int arr[MAX_LIST_SIZE];
+	element arr[MAX_LIST_SIZE];
 	int size;
 
 public:
@@ -18,33 +18,32 @@ public:
 	ArrayListType() { init(); }
 	bool is_empty() { return size == 0; }
 	bool is_full() { return size == MAX_LIST_SIZE; }
-	int get_entry(int index);
+	element get_entry(int index);
 	void print_list();
-	void insert_Last(int item);
-	void insert(int index, int item);
-	int deleteItem(int index);
+	void insert_Last(element item);
+	void insert(int index, element item);
+	element deleteItem(int index);
 };
 
 #pragma region ArrayListType_Define
 //해당 인덱스 자료 가져오기
-int ArrayListType::get_entry(int index) {
+element ArrayListType::get_entry(int index) {
 	if (index < 0 || index > MAX_LIST_SIZE)
 		error("위치 입력오류");
 	else
 		return arr[index];
 }
 void ArrayListType::print_list() {
-	cout << "size : " << size << " ";
 	for (int i = 0; i < size; i++)
 		cout << arr[i] << "->";
 	cout << endl;
 }
-void ArrayListType::insert_Last(int item) {
+void ArrayListType::insert_Last(element item) {
 	if (size >= MAX_LIST_SIZE)
 		error("리스트 오버플로우");
 	arr[size] = item; size++;
 }
-void ArrayListType::insert(int index, int item) {
+void ArrayListType::insert(int index, element item) {
 	if (!is_full() && index >= 0 && index <= size) {	//2 3 4, 0 1 3 4 
 		for (int i = size - 1; i >= index; i--)
 			arr[i + 1] = arr[i];
@@ -53,13 +52,12 @@ void ArrayListType::insert(int index, int item) {
 	}
 }
 
-int ArrayListType::deleteItem(int index) {
-	int result;
+element ArrayListType::deleteItem(int index) {
+	element result;
 	if (index >= 0 && index < size) {
-		int result = arr[index];
-		for (int i = index; i < size - 1; i++) {// 3, 0 1 2 3 4 , 0 1 2 4
-			arr[i] = arr[i + 1];
-		}
+		element result = arr[index];
+		for (int i = index; i < size - 1; i++) // 3, 0 1 2 3 4 , 0 1 2 4
+			arr[index] = arr[index + 1];
 		size--;
 		return result;
 	}
@@ -75,7 +73,7 @@ int main()
 	list.insert(0, 10); list.print_list();
 	list.insert(0, 20); list.print_list();
 	list.insert(0, 30); list.print_list();
-	list.insert_Last(40); list.print_list();
+	//list.insert_Last(40); list.print_list();
 	list.deleteItem(0); list.print_list();
 
 	return 0;
