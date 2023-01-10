388903531908f2cbab70e490ee2670a88ef87120
KangDHGit <eogks0323@gmail.com>
2023-01-10 11:41:19 +0900

    01/10 p187 단순연결리스트 완료

--- algorithm/Linkedd_List1/p187_singly_linked_list.cpp
+++ algorithm/Linkedd_List1/p187_singly_linked_list.cpp
@@ -34,22 +34,68 @@ public:
 		return head;
 	}
 
+	static ListNode* insert(ListNode* head, ListNode* pre, element data) {
+		ListNode* new_Node = new ListNode;
+		new_Node->data = data;
+		new_Node->link = pre->link;
+		pre->link = new_Node;
+		return head;
+	}
+
+	static ListNode* delete_first(ListNode* head) {
+		ListNode* removed = head;
+		if (removed == NULL) {
+			cout << "매개변수가 NULL 입니다." << endl;
+			return NULL;
+		}
+		head = head->link;
+		delete removed;
+		return head;
+	}
+
+	static ListNode* delete_Node(ListNode* head, ListNode* pre) {
+		ListNode* removed = pre->link;
+		pre->link = removed->link;
+		delete removed;
+		return head;
+	}
+
+	static void print_list(ListNode* head) {
+		for (ListNode* iter = head; iter != NULL; iter = iter->link)
+			cout << iter->data << "->";
+		cout << "NULL" << endl;
+	}
+	//도전과제
+	static element get_entry(ListNode* head, int index) {
+		if (head == NULL) { cout << "리스트가 NULL 입니다." << endl; return element{ -1 }; }
+		ListNode* resultNode = head;
+		for (int i = 0; i < index; i++)
+		{
+			if (resultNode == NULL) { cout << "잘못된 인덱스 값입니다." << endl; return element{ -1 }; }
+			resultNode = resultNode->link;
+		}
+		return resultNode->data;
+	}
 };
 
 int main()
 {
-	/*ListNode* node = new ListNode;
-	node->data = 10;
-	cout << node->data << endl;
-	node->insert_first(20);
-	cout << node->data << " " << node->link->data << endl;
-	node->insert_first(30);
-	cout << node->data << " " << node->link->data << " " << node->link->link->data << endl;*/
+	ListNode* head = NULL;
+	for (int i = 0; i < 5; i++) {
+		head = ListNode::insert_first(head, i);
+		ListNode::print_list(head);
+	}
 
-	ListNode* node0 = new ListNode;
-	node0->data = 10;
-	cout << node0->data << endl;
-	ListNode* node1 = ListNode::insert_first(node0, 20);
-	cout << node1->data << endl;
+	ListNode* pre = head->link->link;		//4->3->2, 즉 2가 들어있는 노드
+	head = ListNode::insert(head, pre, 10);	//2가 들어있는 노드뒤에 10 삽입
+	ListNode::print_list(head);				//4->3->2->10->1->0
+	element data = ListNode::get_entry(head, 3);
+	cout << "3번째 인덱스는 : " << data << "입니다." << endl;
+
+	head = ListNode::delete_first(head);
+	ListNode::print_list(head);				//3->2->10->1->0;
+	ListNode::delete_Node(head, pre);		//pre의 다음노드 삭제
+	ListNode::print_list(head);				//3->2->1->0
+
 	return 0;
 }
