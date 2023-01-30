71865ac2220575641b12f22381ff60078a0d76e6
KangDHGit <eogks0323@gmail.com>
2023-01-30 16:30:44 +0900

    01/30 chapter8 p280 이진트리 큐활용 레벨순회 구현중

---
+++ algorithm/Chapter8_Tree/p280_level_order.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+struct TreeNode
+{
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+#define MAX_SIZE 10
+
+class QueueType {
+	TreeNode* data[MAX_SIZE];
+	int front;
+	int rear;
+
+public:
+	QueueType() { front = -1; rear = -1; }
+	bool IsEmpty() { return front == rear; }
+	bool IsFull() { return (rear + 1) % MAX_SIZE == front; }
+	void Enqueue(TreeNode* item) {
+		if (IsFull()) {
+			cout << "큐가 포화상태 입니다." << endl; return;
+		}
+		rear = (rear + 1) % MAX_SIZE;
+		data[rear] = item;
+	}
+	TreeNode* Dequeue() {
+		if (IsEmpty()) {
+			cout << "큐가 공백상태 입니다." << endl; return nullptr;
+		}
+		front = (front + 1) % MAX_SIZE;
+		return data[front];
+	}
+};
+
+void LevelOrder(QueueType queue, TreeNode* root) {
+	if (root == nullptr) return;
+	queue.Enqueue(root);
+	TreeNode* order = nullptr;
+	while (!queue.IsEmpty())
+	{
+		order = queue.Dequeue();
+		cout << "[" << order->data << "] ";
+		if (order->left != nullptr)
+			queue.Enqueue(root->left);
+		if (order->right != nullptr)
+			queue.Enqueue(root->right);
+	}
+}
+
+//			  15
+//		4			20
+//	1			16		25
+int main()
+{
+	TreeNode n1 = { 1, nullptr, nullptr };
+	TreeNode n2 = { 4, &n1, nullptr };
+	TreeNode n3 = { 16, nullptr, nullptr };
+	TreeNode n4 = { 25, nullptr, nullptr };
+	TreeNode n5 = { 20, &n3, &n4 };
+	TreeNode n6 = { 15, &n2, &n5 };
+	TreeNode* root = &n6;
+	
+	QueueType queue;
+	LevelOrder(queue, root);
+	cout << endl;
+
+	return 0;
+}
