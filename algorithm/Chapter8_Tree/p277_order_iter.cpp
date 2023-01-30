6b99570e787b939ba8c6b4cefad952c33cdfb751
KangDHGit <eogks0323@gmail.com>
2023-01-30 14:57:20 +0900

    01/30 chapter8 p277 이진트리 스택활용 반복적 순회 구현 완료

--- algorithm/Chapter8_Tree/p277_order_iter.cpp
+++ algorithm/Chapter8_Tree/p277_order_iter.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+//반복적 순회
 
 using std::cout;
 using std::endl;
@@ -18,20 +19,47 @@ class NodeStack {
 public:
 	NodeStack() { top = -1; }
 	void Push(TreeNode* node) {
-		top++;
-		data[top] = node;
+		if (top < MAX_SIZE - 1) {
+			top++;
+			data[top] = node;
+		}
 	}
 	TreeNode* Pop() {
-		if (top == -1) { cout << "스택이 비어있습니다." << endl; exit(-1); }
-		return data[top--];
+		TreeNode* result = nullptr;
+		if (top >= 0)
+			result = data[top--];
+		return result;
 	}
 };
 
-void InOrderIter(TreeNode* head) {
-
+void InOrderIter(NodeStack stack, TreeNode* root) {
+	while (true)
+	{
+		for (; root != nullptr; root = root->left)
+			stack.Push(root);
+		root = stack.Pop();
+		if (root == nullptr)	//Pop함수에서 nullptr을 리턴할때
+			break;
+		cout << "[" << root->data << "] ";
+		root = root->right;
+	}
 }
 
+//			  15
+//		4			20
+//	1			16		25
 int main()
 {
+	TreeNode n1 = { 1, nullptr, nullptr };
+	TreeNode n2 = { 4, &n1, nullptr };
+	TreeNode n3 = { 16, nullptr, nullptr };
+	TreeNode n4 = { 25, nullptr, nullptr };
+	TreeNode n5 = { 20, &n3, &n4};
+	TreeNode n6 = { 15, &n2, &n5 };
+	TreeNode* root = &n6;
+	
+	NodeStack stack;
+	InOrderIter(stack, root);
+	cout << endl;
 	return 0;
 }
